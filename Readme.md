c6bbcdab47872bb2e9f4f9b878e0ce8d12760fb9
martin76ec <larreaamartin@gmail.com>
2022-11-19 13:05:33 -0500

    feat: improve static closures feat: add readme

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Rust calculator
+
+## Execution steps
+
+1. Clone repository `git clone https://github.com/martin76ec/rust-calc`
+2. Run `cargo run`
+
+## How to use
+
+- Peform adds,substractions, divitions and multiplications to the any numbers, e.g: 
+`10*2*44+2/2+4*23`.
+- Type exit to leave the program
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ static RE_ADD: &str = r"(\d+)\s?\+\s?(\d+)";
 static RE_DIV: &str = r"(\d+)\s?/\s?(\d+)";
 static RE_MUL: &str = r"(\d+)\s?\*\s?(\d+)";
 static RE_SUB: &str = r"(\d+)\s?\-\s?(\d+)";
-static ADD: fn(l_val: i32, r_val: i32) -> i32 = |l_val: i32, r_val: i32| -> i32 { l_val + r_val };
-static DIV: fn(l_val: i32, r_val: i32) -> i32 = |l_val: i32, r_val: i32| -> i32 { l_val / r_val };
-static MUL: fn(l_val: i32, r_val: i32) -> i32 = |l_val: i32, r_val: i32| -> i32 { l_val * r_val };
-static SUB: fn(l_val: i32, r_val: i32) -> i32 = |l_val: i32, r_val: i32| -> i32 { l_val - r_val };
+
+static ADD: fn(i32, i32) -> i32 = |l_val, r_val| -> i32 { l_val + r_val };
+static DIV: fn(i32, i32) -> i32 = |l_val, r_val| -> i32 { l_val / r_val };
+static MUL: fn(i32, i32) -> i32 = |l_val, r_val| -> i32 { l_val * r_val };
+static SUB: fn(i32, i32) -> i32 = |l_val, r_val| -> i32 { l_val - r_val };
+
+static EXIT: fn(String) -> String = |expr: String| -> String {
+    let caps = Regex::new(r"exit").unwrap().captures(&expr);
+    if caps.is_none() { return expr };
+    return caps.unwrap().get(0).unwrap().as_str().parse().unwrap();
+};
 
 fn make_operation(rgx: String, op: fn(l_val: i32, r_val: i32) -> i32) -> impl Fn(String) -> String {
     return move |expr: String| -> String {
@@ -36,14 +43,18 @@ fn main() {
     let multiply = make_operation(RE_MUL.to_string(), MUL);
     let substract = make_operation(RE_SUB.to_string(), SUB);
 
-    println!("Please, add your expression:");
-    let mut expression = String::new();
-    std::io::stdin().read_line(&mut expression).unwrap();
+    loop {
+        println!("Please, add your expression:");
+        let mut expression = String::new();
+        std::io::stdin().read_line(&mut expression).unwrap();
 
-    expression = multiply(expression);
-    expression = divide(expression);
-    expression = add(expression);
-    expression = substract(expression);
+        expression = multiply(expression);
+        expression = divide(expression);
+        expression = add(expression);
+        expression = substract(expression);
+        expression = EXIT(expression);
 
-    println!("Result: {}", expression);
+        println!("Result: {}", expression);
+        if expression == "exit" { break };
+    }
 }
